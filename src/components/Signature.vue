6340137c445d6a5dd0b90e996779c8ac67213004
networkinterfacecontroller <landsoftlydancing@gmail.com>
2024-05-21 14:55:16 -0600

    bump out for logo

--- src/components/Signature.vue
+++ src/components/Signature.vue
@@ -6,15 +6,23 @@ import Card from 'primevue/card'
 
 <template>
     <Card>
-        <template #content>      
-            {{ form.name }} <br />
-            {{ form.role }} @ {{ form.company }} <br />
-            {{ form.website }} <br /> <br />
-            <i class="pi pi-linkedin"></i>&nbsp;
-            <i class="pi pi-instagram"></i>&nbsp;
-            <i class="pi pi-facebook"></i>&nbsp;
-            <i class="pi pi-twitter"></i>&nbsp; <br />
-            {{ form.phone }}
+        <template #content>
+            <div class="grid">
+                <div class="col-2">
+
+                </div>
+                <div class="col-8">
+                    {{ form.name }} <br />
+                    {{ form.role }} @ {{ form.company }} <br />
+                    {{ form.website }}
+                    <br /> <br />
+                    <i class="pi pi-linkedin"></i>&nbsp;
+                    <i class="pi pi-instagram"></i>&nbsp;
+                    <i class="pi pi-facebook"></i>&nbsp;
+                    <i class="pi pi-twitter"></i>&nbsp; <br />
+                    {{ form.phone }}
+                </div>
+            </div>      
         </template>
     </Card>
 </template>
