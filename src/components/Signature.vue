4c78ea18cbb5d6bdac45d712791eec7487ae2761
networkinterfacecontroller <landsoftlydancing@gmail.com>
2024-05-24 12:42:34 -0600

    signature image and socials

--- src/components/Signature.vue
+++ src/components/Signature.vue
@@ -7,22 +7,39 @@ import Card from 'primevue/card'
 <template>
     <Card>
         <template #content>
-            <div class="grid">
-                <div class="col-2">
+            <table class="grid">
+                <tr>
+                <td class="col">
+                    <img :src=form.imgurl height="100" />
+                </td>
 
-                </div>
-                <div class="col-8">
+                <td class="col-8" style="padding-left: 20px; padding-top: 16px;">
                     {{ form.name }} <br />
-                    {{ form.role }} @ {{ form.company }} <br />
-                    {{ form.website }}
+                    <span v-if="form.role && form.company">
+                        {{ form.role }} @ {{ form.company }}
+                    </span> <br />
+                    <a :href="form.website">{{ form.website }}</a>
                     <br /> <br />
-                    <i class="pi pi-linkedin"></i>&nbsp;
-                    <i class="pi pi-instagram"></i>&nbsp;
-                    <i class="pi pi-facebook"></i>&nbsp;
-                    <i class="pi pi-twitter"></i>&nbsp; <br />
+                    <span style="padding-left: 3px;">
+                        <span v-if="form.linkedin">
+                            <a :href="form.linkedin"><img height="15" src="../assets/linkedin-in.png"></img></a>&nbsp;
+                        </span>
+                        <span v-if="form.instagram">
+                            <a :href="form.instagram"><img height="15" src="../assets/instagram.png"></img></a>&nbsp;
+                        </span>
+                        <span v-if="form.facebook">
+                            <a :href="form.facebook"><img height="15" src="../assets/facebook.png"></img></a>&nbsp;
+                        </span>
+                        <span v-if="form.twitter">
+                            <a :href="form.twitter"><img height="15" src="../assets/x-twitter.png"/></a>&nbsp;
+                        </span>
+                    </span>
+                    
+                    <br />
                     {{ form.phone }}
-                </div>
-            </div>      
+                </td>
+            </tr>
+        </table>      
         </template>
     </Card>
 </template>
